ffbdc88da3b51968e7e6cd211f1e104718fa6f8b
JonathnSparePartsFuller <jonefuller@gmail.com>
2022-04-06 15:58:28 -0700

    Finished the script and did worked on css

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ class App extends Component {
     this.state = {
       // "phrase" is the text entered by the user - right now there are test words hard coded to make the process of testing your code faster and easier
       // ACTION ITEM: when you are ready for your full user experience, delete the test words so phrase is assigned an empty string
-      phrase: "alpha through yummy squeal queen fry",
+      phrase: "",
       // "phraseTranslated" is what the user will see appear on the page as Pig Latin, it starts as the preset message and updates when your user clicks the "submit" button
-      phraseTranslated: "This is where your translated sentence will appear."
+      phraseTranslated: ""
     }
   }
 
@@ -118,8 +118,8 @@ class App extends Component {
     // this method restarts the game by setting the original state
     // ACTION ITEM: when you are ready for your full user experience, delete the test words in phrase so that is assigned an empty string
     this.setState({
-      phrase: "alpha through yummy squeal queen fry",
-      phraseTranslated: "This is where your translated sentence will appear."
+      phrase: "",
+      phraseTranslated: ""
     })
   }
 
@@ -140,27 +140,29 @@ class App extends Component {
     return (
       <>
         <h1>Pig Latin Translator</h1>
+        <div className="inputArea">
         <img
           src={butcherPig}
           alt="pig with butcher cut names in pig latin"
           className="butcherPig"
         />
-        <div className="inputArea">
-          <h4>Enter phrase to be translated:</h4>
+          <h4>Begin your Pig Latin Journey!</h4>
           {/* user input field - every DOM event that happens in the input will call the handleChange method and update state */}
           <input
             type="text"
             className="userInput"
             onChange={this.handleInput}
             value={this.state.phrase}
+            placeholder="Enter text to translate:"
           />
           <br />
           {/* button that called the setUpPreventDefault method which calls the myPigLatinCodeHere method */}
           <button onClick={this.setUpPreventDefault}>Submit</button>
           <button onClick={this.restartGame}>Clear</button>
         </div>
+        <h4>eginBay ouryay igPay atinLay ourneyJay</h4>
         <p>{this.state.phraseTranslated}</p>
-        <footer>Coded by ~your name here~</footer>
+        <footer>Coded by Jonathan & Dustin</footer>
       </>
     )
   }
